cc49e65cd15cc01e10bb59e23e1e00e85a0d0bc4
Dave Reed <dave256@mac.com>
2014-11-24 19:23:33 -0500

    Initial Commit

---
+++ SelectValueTableViewController/SelectValueTableViewController.h
@@ -0,0 +1,19 @@
+//
+//  SelectValueTableViewController.h
+//  SelectValueTableViewController
+//
+//  Created by David M Reed on 11/24/14.
+//  Copyright (c) 2014 David M Reed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SelectValueTableViewController.
+FOUNDATION_EXPORT double SelectValueTableViewControllerVersionNumber;
+
+//! Project version string for SelectValueTableViewController.
+FOUNDATION_EXPORT const unsigned char SelectValueTableViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SelectValueTableViewController/PublicHeader.h>
+
+
